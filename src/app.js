691bbc72fb1f68798eea90f2dcb6a5b6d5b95c40
Silvia Moraga <110190215+SilviaMoraga@users.noreply.github.com>
2024-05-17 14:56:18 +0000

    Generador excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+  console.log("Hello rigo from the console! ");
 };
+
+function generateExcuse() {
+  let who = ["The dog", "The fish", "The neighbor", "My grandma"];
+  let action = ["ate", "broke", "burned", "crushed"];
+  let what = ["my phone", "your car", "my keys", "my homework"];
+  let when = [
+    "yesterday",
+    "during my lunch",
+    "when I was taking a nap",
+    "while I was in the shower"
+  ];
+
+  return (
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)]
+  );
+}
